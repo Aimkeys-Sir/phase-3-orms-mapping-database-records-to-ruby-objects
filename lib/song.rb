30b51b0d4591b0e51ba674beacaf4a52a8a9a584
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-08-25 15:39:49 +0300

    commit tree bdd178bcfb8c18ceb39a4116a2f7405295e897db

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,22 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    self.new(id:row[0],name:row[1],album:row[2])
+  end
+
+  def self.all
+    DB[:conn].execute("SELECT * FROM songs").map do |row|
+       self.new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    query=<<-SQL
+    SELECT * FROM songs
+    WHERE name=?
+    LIMIT 1
+    SQL
+    self.new_from_db(DB[:conn].execute(query,name).first)
+  end
 end
